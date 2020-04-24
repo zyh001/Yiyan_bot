ea54035c0aac9133d66603ec86d4a12500838292
root <stackzhao@gmail.com>
2020-04-24 05:36:37 +0000

    添加数据库自动更新功能，依赖Python3 - telethon

--- get.sh
+++ get.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-
+set -e
 list=$(cat ./db.txt | wc -l)
 num=$(($RANDOM%list+1))
-
+update_db(){
+	set -e
+	python3 ./getmsg.py >/dev/null 2>&1
+	awk '!x[$0]++' db.txt | sed '/^[[:space:]]*$/d' > db1.txt
+	mv ./db1.txt db.txt
+	echo 0 > /tmp/getyiyan.temp
+}
+if [[ -f /tmp/getyiyan.temp ]];then
+	i=$(cat /tmp/getyiyan.temp)
+	if [[ ${i} == "10" ]];then
+		(update_db &)
+	else
+		i=$((i+1))
+		echo ${i} > /tmp/getyiyan.temp
+	fi
+else
+	echo 0 > /tmp/getyiyan.temp
+fi
 echo -e $(sed -n "${num}p" ./db.txt)
+exit 0
---
+++ getmsg.py
@@ -0,0 +1,20 @@
+from telethon.sync import TelegramClient, utils
+
+api_id = '1089079'
+api_hash = '7799f8caaabbc8087bf6614bdd7cb069'
+bot = TelegramClient('🐶机器人', api_id, api_hash)
+channel = 'https://t.me/qinghua_box'
+ 
+async def main():
+    messages = bot.iter_messages(channel, limit=100)
+    msges = '';
+    async for message in messages:
+        msg = str(message.message)
+        msge = msg.replace("\r", r"\r").replace('\n', r'\n')
+        # print(msge)
+        msges = msges + msge + "\n"
+    with open('./db.txt', 'a+') as file:
+        file.write(msges)
+
+with bot:
+    bot.loop.run_until_complete(main())
