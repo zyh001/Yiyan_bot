c88ba82dcd64220c8991449c72a2276b57c3fdcb
root <stackzhao@gmail.com>
2020-04-28 11:51:56 +0800

    fix bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps : [{
     name: 'qhyiyanbot',
-    script: 'sudo npm run start',
+    script: 'npm run start',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '256M',
     env: {
       NODE_ENV: 'development'
     },
